74a76cc4664d6a7a74981d7aaf32ab73eb711e30
msiwiec <msiwiec@yahoo.com>
2014-08-28 14:46:28 -0600

    Add Golem directory structure and initial basic smoke test

---
+++ Golem.PageObjects.RG/HomePage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MbUnit.Framework;
+using OpenQA.Selenium.Interactions;
+using ProtoTest.Golem.Core;
+using ProtoTest.Golem.WebDriver;
+using OpenQA.Selenium;
+using ProtoTest.Golem.WebDriver.Elements;
+using ProtoTest.Golem.WebDriver.Elements.Types;
+
+namespace Golem.PageObjects.RG
+{
+    public class HomePage : BasePageObject
+    {
+        //LoggedOutHeader loggedOutHeader = new LoggedOutHeader();
+        //LoggedInHeader loggedInHeader = new LoggedInHeader();
+        Element RG_Logo = new Element("RG LOGO", By.ClassName("logo"));
+        public static HomePage OpenHomePage()
+        {
+            WebDriverTestBase.driver.Navigate().GoToUrl(Config.GetConfigValue("EnvUrl", "http://revolutiongolf-integration-2014.bluemod.us/"));
+            return new HomePage();
+        }
+
+        public override void WaitForElements()
+        {
+            RG_Logo.WaitUntil().Visible();
+        }
+    }
+}
---
+++ Golem.Tests.RG/SmokeTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoTest.Golem.Core;
+using ProtoTest.Golem.WebDriver;
+using Golem.PageObjects.RG;
+using MbUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Golem.Tests.RG
+{
+    [TestFixture]
+    public class SmokeTest : WebDriverTestBase
+    {
+        [Test, Category("Smoke Test")]
+        public void VerifyApplicationUp()
+        {
+            HomePage.OpenHomePage();
+        }
+
+    }
+}
